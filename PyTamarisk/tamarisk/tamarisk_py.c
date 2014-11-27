b8fefffc860c6314749f88240d6e72c538df6a6b
Andrew Tridgell <tridge@samba.org>
2014-11-27 13:46:54 +1100

    added data capture to file

--- PyTamarisk/tamarisk/tamarisk_py.c
+++ PyTamarisk/tamarisk/tamarisk_py.c
@@ -7,6 +7,8 @@
 #include <stdint.h>
 #include <string.h>
 #include <arv.h>
+#include <sys/time.h>
+#include <time.h>
 
 #define NUM_CAMERA_HANDLES 1
 
@@ -24,6 +26,8 @@ static ArvCamera *camera;
 static ArvStream *stream;
 static const uint32_t width = 640;
 static const uint32_t height = 480;
+
+static bool save_pgm_frame(const uint8_t *data0);
 
 /*
   write one camera device register
@@ -113,7 +117,15 @@ tamarisk_capture(PyObject *self, PyObject *args)
 	int status;
 
 	Py_BEGIN_ALLOW_THREADS;
-
+	ArvBuffer *buffer;
+	buffer = arv_stream_try_pop_buffer(stream);
+	if (buffer != NULL) {
+		if (buffer->status == ARV_BUFFER_STATUS_SUCCESS) {
+                        unsigned BPP = ARV_PIXEL_FORMAT_BIT_PER_PIXEL(buffer->pixel_format);
+                        save_pgm_frame(buffer->data);
+                }
+		arv_stream_push_buffer (stream, buffer);
+	}
 	Py_END_ALLOW_THREADS;
 	
 	if (status < 0) {
@@ -140,6 +152,74 @@ tamarisk_close(PyObject *self, PyObject *args)
 	Py_RETURN_NONE;
 }
 
+
+
+static const char *save_basename(void)
+{
+    struct timeval tv;
+    struct tm *tm;
+    time_t t;
+    unsigned hundredths;
+    static char namebuf[100];
+
+    gettimeofday(&tv, NULL);
+    t = tv.tv_sec;
+    tm = gmtime(&t);
+    hundredths = tv.tv_usec / 10000;
+
+    strftime(namebuf, sizeof(namebuf), "raw%Y%m%d%H%M%S", tm);
+    snprintf(namebuf+strlen(namebuf), sizeof(namebuf)-strlen(namebuf), "%02uZ", hundredths);
+    return namebuf;
+}
+
+/*
+  save a .pgm greyscale image, auto-incrementing frame number
+ */
+static bool save_pgm_frame(const uint8_t *data0)
+{
+	int fd, ret;
+	char *filename = NULL;
+	uint16_t *data;
+        uint32_t size = width*height*2;
+
+	if (asprintf(&filename, "%s.pgm", save_basename()) <= 0) {
+		return false;
+	}
+
+	printf("saving %s shape=%ux%u\n", 
+	       filename,
+	       (unsigned)width,
+	       (unsigned)height);
+  
+	fd = open(filename, O_WRONLY|O_CREAT|O_TRUNC, 0644);
+	if (fd == -1) {
+		perror(filename);
+		free(filename);
+		return false;
+	}
+	free(filename);
+	dprintf(fd,"P5\n%u %u\n%u\n", 
+		width, height, 65535);
+
+	data = malloc(size);
+	memcpy(data, data0, size);
+
+	// fix byte order if needed fpr pgm file
+	if (__BYTE_ORDER == __LITTLE_ENDIAN) {
+	  // needs byte swap
+	  swab(data, data, size);
+	}
+
+	ret = write(fd, data, size);
+	free(data);
+	if (ret != size) {
+		close(fd);
+		printf("write failed ret=%d\n", ret);
+		return false;
+	}
+	close(fd);
+	return true;
+}
 
 static PyMethodDef TamariskMethods[] = {
   {"open", tamarisk_open, METH_VARARGS, "Open a tamarisk camera. Returns handle"},
